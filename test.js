50a77a52de2f1eed25625a5fec9bd5437e471428
misaku <to41207052@yahoo.co.jp>
2023-12-31 12:49:16 +0900

    ww

---
+++ test.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $(document.body).hide().fadeIn("slow");
+    });
+});
---
+++ test.ts
@@ -0,0 +1,29 @@
+// オブジェクトのキーを取得する
+
+export interface FormValues {
+    last_name: string | null;
+    first_name: string | null;
+    gender: number | null;
+    birthday: string | null;
+}
+
+const setValue = <K extends keyof FormValues>(
+    obj: FormValues, // オブジェクトを引数として追加
+    val: FormValues[K],
+    key: K,
+) => {
+    obj[key] = val; // 修正: 関数の引数で渡されたオブジェクトに対して値を設定
+};
+
+// 使用例
+const example: FormValues = {
+    last_name: "Smith",
+    first_name: "John",
+    gender: 1,
+    birthday: "1990-01-01",
+};
+
+setValue(example, "Doe", "last_name");
+setValue(example, 2, "gender");
+
+console.log(example);  // 例: { last_name: 'Doe', first_name: 'John', gender: 2, birthday: '1990-01-01' }
