8b45d87f26bce817e8029a4097cb36f9f2c37aa1
to41207052 <to41207052@gmail.com>
2023-12-31 18:16:09 +0900

    haho

--- test.js
+++ test.js
@@ -1,5 +1,27 @@
-$(document).ready(function(){
-    $('button').click(function(){
-        $(document.body).hide().fadeIn("slow");
-    });
-});
+function randomRoto(x) {
+    switch (x) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+            console.log("".concat(x, ":\u4E00\u6841\u3067\u3059\u306D\uFF01"));
+            break;
+        case 11:
+        case 22:
+        case 33:
+        case 44:
+        case 55:
+            console.log("".concat(x, ":\u30AD\u30EA\u756A\u3067\u3059\u306D\uFF01"));
+            break;
+        default:
+            console.log("".concat(x, "\u30CF\u30BA\u30EC"));
+            break;
+    }
+}
+var a = Math.floor(Math.random() * 60);
+randomRoto(a);
